7e71846fbbf0622d96ad7e2dd269127bc8ee6233
closekn <close.me.kn@gmail.com>
2019-10-16 21:58:29 +0900

    add algo/union_find

---
+++ algo/union_find.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+
+struct UnionFind {
+  vector<int> par;
+
+  UnionFind(int N) : par(N) {
+    for ( int i = 0; i < N; i++ ) { par[i] = i; }
+  }
+
+  int root(int x) { 
+    if ( par[x] == x ) { return x; }
+    return par[x] = root(par[x]);
+  }
+
+  void unite(int x, int y) {
+    int rx = root(x);
+    int ry = root(y);
+    if ( rx == ry ) { return; }
+    par[rx] = ry;
+  }
+
+  bool same(int x, int y) {
+    int rx = root(x);
+    int ry = root(y);
+    return rx == ry;
+  }
+};
